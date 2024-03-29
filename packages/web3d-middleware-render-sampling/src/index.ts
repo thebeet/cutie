164ea0a2c3a3f48e19ee51e844c5e572c67df195
xiangguangte <xiangguangte@baidu.com>
2024-03-29 11:27:53 +0800

    + opt downsampling

--- packages/web3d-middleware-render-sampling/src/index.ts
+++ packages/web3d-middleware-render-sampling/src/index.ts
@@ -1,90 +1,65 @@
 import * as THREE from 'three';
 import { useAdvanceDrama } from '@cutie/web3d';
-import { useRafFn } from '@vueuse/core';
-import { computed } from 'vue';
+import { useMemoize, useRafFn } from '@vueuse/core';
+import { watch, computed, ref } from 'vue';
 
 export const useMiddleware = () => {
     const { controls, scene, frames, page, activeFrames } = useAdvanceDrama();
-
-    const maxPointsCounts = Math.max(0, ...page.data.frames.map(f => f.points));
-
+    const isSampled = ref(false);
     const activePoints = computed(() => activeFrames.value.reduce(
         (acc, frame) => acc + page.data.frames[frame.index - 1].points, 0));
-
     const thredhold = 1_000_000;
-
-    const steps = [
-        2, 3, 4, 5, 6, 8, 10,
-        12, 14, 16, 18, 20,
-        25, 30, 35, 40, 45, 50,
-    ];
-    const sampleAttributes = new Map(steps.map(step => {
-        const n = Math.floor(maxPointsCounts / step);
-        const arr = new Uint32Array(n);
-        for (let i = 0; i < n; i++) {
+    const getAttrs = useMemoize((step: number) => {
+        const arr = new Uint32Array(thredhold);
+        for (let i = 0; i < thredhold; i++) {
             arr[i] = i * step;
         }
-        return [step, new THREE.BufferAttribute(arr, 1)];
-    }));
-
-    scene.addEventListener('downsampling', () => {
-        if (activePoints.value <= thredhold) {
-            return;
-        }
-        const t = Math.floor(activePoints.value / thredhold);
-        const step = steps.reduce((acc, item) => t <= item ? Math.min(acc, item) : acc, 50);
-        const sampleAttribute = sampleAttributes.get(step)!;
-        frames.forEach(frame => {
-            if (frame.points) {
-                const geometry = frame.points.geometry;
-                const n = geometry.attributes.position.count;
-                geometry.setIndex(sampleAttribute);
-                geometry.setDrawRange(0, Math.floor(n / step));
-            }
-        });
-    });
-    scene.addEventListener('resetsampling', () => {
-        frames.forEach(frame => {
-            if (frame.points) {
-                const geometry = frame.points.geometry;
-                geometry.setIndex(null);
-                geometry.setDrawRange(0, Infinity);
-            }
-        });
+        return new THREE.BufferAttribute(arr, 1);
     });
 
-    const setPointCloudSampled = (isSampled: boolean) => {
-        // @ts-ignore
-        scene.dispatchEvent({ 'type': isSampled ? 'downsampling' : 'resetsampling' });
-    };
-    let sampling = 0;
-    controls.domElement.addEventListener('mousedown', () => {
-        if (controls.enabled && sampling < 6) {
-            sampling = 6;
-            setPointCloudSampled(true);
-        }
-    });
-    controls.domElement.addEventListener('mouseup', () => {
-        if (controls.enabled && sampling !== 0) {
-            sampling = 0;
-            setPointCloudSampled(false);
+    watch(isSampled, (value) => {
+        if (value) {
+            if (activePoints.value <= thredhold) {
+                return;
+            }
+            const step = Math.ceil(activePoints.value / thredhold);
+            const index = getAttrs(step);
+            frames.forEach(frame => {
+                if (frame.points) {
+                    const geometry = frame.points.geometry;
+                    const n = geometry.attributes.position.count;
+                    geometry.setIndex(index);
+                    geometry.setDrawRange(0, Math.floor(n / step));
+                }
+            });
+        } else {
+            frames.forEach(frame => {
+                if (frame.points) {
+                    const geometry = frame.points.geometry;
+                    geometry.setIndex(null);
+                    geometry.setDrawRange(0, Infinity);
+                }
+            });
             scene.update();
         }
     });
-    controls.addEventListener('change', () => {
-        if (sampling < 5) {
-            if (sampling === 0) {
-                setPointCloudSampled(true);
-            }
-            sampling = 5;
-        }
-    });
 
+    let sampling = 0;
+    controls.domElement.addEventListener('mousedown', () => {
+        sampling = 6;
+    });
+    controls.domElement.addEventListener('mouseup', () => {
+        sampling = 0;
+        isSampled.value = false;
+    });
+    controls.addEventListener('change', () => {
+        sampling = Math.max(sampling, 5);
+        isSampled.value = true;
+    });
     useRafFn(() => {
         if ((sampling > 0) && (sampling <= 5)) {
             if (--sampling === 0) {
-                setPointCloudSampled(false);
-                scene.update();
+                isSampled.value = false;
             }
         }
     });
